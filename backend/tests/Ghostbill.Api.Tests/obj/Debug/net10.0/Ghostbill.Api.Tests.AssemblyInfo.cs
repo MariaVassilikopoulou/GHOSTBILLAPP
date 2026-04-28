e282cfe9008bd337c73306cf8d55554cbcf6de41
EliasKB <eliaskamyab@gmail.com>
2026-04-28 21:11:43 +0200

    commit before skills

--- backend/tests/Ghostbill.Api.Tests/obj/Debug/net10.0/Ghostbill.Api.Tests.AssemblyInfo.cs
+++ backend/tests/Ghostbill.Api.Tests/obj/Debug/net10.0/Ghostbill.Api.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ghostbill.Api.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce928a4031909336ab37d2d3163b45560ac750d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ghostbill.Api.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ghostbill.Api.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
